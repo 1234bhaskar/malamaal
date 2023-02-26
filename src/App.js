9ca8abb6cf8df0fafd46e63cea552e998e429d7e
1234bhaskar <bhaskar2021chand@gmail.com>
2023-02-26 22:26:52 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import axios from "axios";
 import Coin from "./Coin";
 import { React, useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 //https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false
 
 const App = () => {
+  const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0(); //for login
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
   useEffect(() => {
@@ -31,6 +33,34 @@ const App = () => {
   );
   return (
     <div className="coin-app flex flex-col items-center mt-5 overflow-y-auto no-scrollbar">
+      {isAuthenticated ? (
+        <button
+          onClick={() =>
+            logout({ logoutParams: { returnTo: window.location.origin } })
+          }
+          className="border rounded-sm sm:absolute sm:right-10 p-1 hover:bg-gradient-to-r to-blue-400 from-purple-600 duration-1000 ease-in-out hover:scale-105"
+        >
+          Log Out
+        </button>
+      ) : (
+        <button
+          onClick={() => loginWithRedirect()}
+          className="border rounded-sm sm:absolute sm:right-10 p-1 hover:bg-gradient-to-r to-blue-400 from-purple-600 duration-500 ease-in-out hover:scale-105"
+        >
+          Log In
+        </button>
+      )}
+      {isAuthenticated && (
+        <div className="sm:absolute sm:left-0 sm:m-1 sm:flex flex items-center">
+          <img
+            src={user.picture}
+            alt={user.name}
+            className="h-8 rounded-2xl mx-2"
+          />
+          <div>{user.name}</div>
+        </div>
+      )}
+
       <div className="coin-search flex flex-col items-center">
         <h1 className="coin-text text-3xl sm:text-6xl">Search a currency</h1>
         <form>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Auth0Provider
+    domain="dev-c4vh55kispqo87u0.us.auth0.com"
+    clientId="GJLw40RwUryRuVmnayhrDlcDHpFxH5Pa"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <App />
+  </Auth0Provider>
+);
